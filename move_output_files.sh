bed31b3f41a19981c4023b88d07a003a73097aa0
eric <cgx1997@outlook.com>
2019-05-17 17:08:15 -0700

    added comment

--- move_output_files.sh
+++ move_output_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# this script moves training data from bin to somewhere else
+# Usage : param - folder where you want to store training data
 
 mv bin/benign_samples* $1
 mv bin/malware_samples* $1
