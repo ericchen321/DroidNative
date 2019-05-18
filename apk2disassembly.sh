f2dd3e649bb1009554d1daa1c376bf3d2908eed1
eric <cgx1997@outlook.com>
2019-05-18 12:03:52 -0700

    use 2 processes for production - one for benign and one for malware

--- apk2disassembly.sh
+++ apk2disassembly.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
 # this script converts all apk files in given directory to disassembly
 # usage:
-# first param: path to directory where apk files are stored
-# second param: name of running emulator
-# 3rd param: path to output directory
+# first param: path to directory where benign apks are stored
+# 2nd param: path to directory where malware apks are stored
+# 3rd param: name of running emulator
+# 4th param: path to output directory where benign txts are stored
+# 5th param: path to output directory where malware txts are stored
 
-if [ "$#" -ne 3 ]; then
-    echo "You are not using it the right way"
+convert () {
+	if [ "$1" = "benign" ]; then
+		in_dir="$in_dir_benign"
+		out_dir="$out_dir_benign"
+		temp="benign_temp"
+	elif [ "$1" = "malware" ]; then
+		in_dir="$in_dir_mal"
+		out_dir="$out_dir_mal"
+		temp="malware_temp"
+	else
+		echo "Error"
+		exit 1
+	fi
+
+	# FIXME: now assume each apk has only one dex file; should add ability to
+	# handle multiple dex files later
+	for APK in  $(ls $in_dir/*.apk |sort -R); do
+		mkdir ./$temp/
+		unzip -o ${APK} classes.dex -d ./$temp/
+		adb -s $emulator shell mkdir /sdcard/$temp
+		adb -s $emulator push ./$temp/classes.dex /sdcard/$temp
+		adb -s $emulator shell dex2oat --dex-file=/sdcard/$temp/classes.dex --oat-file=/sdcard/$temp/classes.dex.oat --instruction-set-features=div
+		adb -s $emulator shell oatdump --oat-file=/sdcard/$temp/classes.dex.oat --output=/sdcard/$temp/$(basename "$APK" .apk).dex.oat.txt
+		adb -s $emulator pull /sdcard/$temp/$(basename "$APK" .apk).dex.oat.txt $out_dir/
+		adb -s $emulator shell rm -rf /sdcard/$temp
+		rm -rf ./$temp/
+	done
+}
+
+if [ "$#" -ne 5 ]; then
+    echo "You are not using it the right way. Please read the comments in script"
     exit 1
 fi
 
-# FIXME: now assume each apk has only one dex file; should add ability to
-# handle multiple dex files later
-for APK in  $(ls $1/*.apk |sort -R); do
-	mkdir ./temp/
-	unzip -o ${APK} classes.dex -d ./temp/
-	adb -s $2 push ./temp/classes.dex /sdcard/
-	adb -s $2 shell dex2oat --dex-file=/sdcard/classes.dex --oat-file=/sdcard/classes.dex.oat --instruction-set-features=div
-	adb -s $2 shell oatdump --oat-file=/sdcard/classes.dex.oat --output=/sdcard/$(basename "$APK" .apk).dex.oat.txt
-	adb -s $2 pull /sdcard/$(basename "$APK" .apk).dex.oat.txt ${3}/
-	adb -s $2 shell rm /sdcard/classes.dex
-	adb -s $2 shell rm /sdcard/classes.dex.oat
-	adb -s $2 shell rm /sdcard/$(basename "$APK" .apk).dex.oat.txt
-	rm -r ./temp/
-done
+in_dir_benign="$1"
+in_dir_mal="$2"
+emulator="$3"
+out_dir_benign="$4"
+out_dir_mal="$5"
+
+convert "malware" &
+convert "benign" &
+wait
