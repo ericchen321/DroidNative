44e0575636210a7919d6ec8e2284ec6cd0a615f3
eric <cgx1997@outlook.com>
2019-06-07 01:10:19 -0700

    added logging to file; now takes one apk and one txt directory

--- apk2disassembly_on-host.py
+++ apk2disassembly_on-host.py
@@ -21,11 +21,10 @@
 #   (Follow the directions at https://source.android.com/source/building.html)
 # 9. Run this script
 # - Script usage:
-# first param: path to directory where benign apks are stored
-# 2nd param: path to directory where malware apks are stored
-# 3rd param: path to output directory where benign txts are stored
-# 4th param: path to output directory where malware txts are stored
-# 5th param (optional): path to Android directory (example: /data/guanxiong/android_source). If not provided then use Android 7 build
+# first param: path to directory where apks are stored
+# 2nd param: path to output directory where txts are stored
+# 3rd param: path to output directory where running log files are stored
+# 4th param (optional): path to Android directory (example: /data/guanxiong/android_source). If not provided then use Android 7 build
 
 import sys
 import os
@@ -44,33 +43,35 @@ def partition_list(list, n):
     for i in range(0, len(list), n):
         yield list[i:i + n]
 
-# Convert given apk file to txt file stored in out_path
-def convert_file(apk_path, out_path):
+# Convert given apk file to txt file stored in out_path; store
+# terminal log to <apk_name>.dex2oat.log and <apk name>.oatdump.log
+# under log_path
+def convert_file(apk_path, out_path, log_path):
     apk_name = os.path.basename(apk_path)
     os.system("cp " + apk_path + " ./" + apk_name)
-    os.system("out/host/linux-x86/bin/dex2oat --runtime-arg -classpath --runtime-arg " + apk_name + " --instruction-set=arm " + "--runtime-arg " + "-Xrelocate " + "--host " + "--boot-image=" + BOOT_IMAGE + " --dex-file=" + apk_name + " --oat-file=" + apk_name + ".dex")
-    os.system("out/host/linux-x86/bin/oatdump --oat-file=" + apk_name + ".dex" + " --instruction-set=arm" + " --output=" + out_path + "/" + apk_name + ".dex" + ".txt")
+    os.system("out/host/linux-x86/bin/dex2oat --runtime-arg -classpath --runtime-arg " + apk_name + " --instruction-set=arm " + "--runtime-arg " + "-Xrelocate " + "--host " + "--boot-image=" + BOOT_IMAGE + " --dex-file=" + apk_name + " --oat-file=" + apk_name + ".dex" + " 2>&1 | tee " + log_path + "/" + apk_name + ".dex2oat.log")
+    os.system("out/host/linux-x86/bin/oatdump --oat-file=" + apk_name + ".dex" + " --instruction-set=arm" + " --output=" + out_path + "/" + apk_name + ".dex" + ".txt" + " 2>&1 | tee " + log_path + "/" + apk_name + ".oatdump.log")
     os.system("rm " + apk_name)
     os.system("rm " + apk_name + ".dex")
 
-# Convert apk files in in_path to txt files stroed in out_path
-def convert_files(in_path, out_path):
+# Convert apk files in in_path to txt files stroed in out_path; store terminal log
+# to log_path
+def convert_files(in_path, out_path, log_path):
     pool = Pool(5)
     samples = glob.glob(in_path + "/*.apk")
     partitions = list(partition_list(samples, 5))
     for partition in partitions:
-        pool.starmap(convert_file, itertools.product(partition, [out_path]))
+        pool.starmap(convert_file, itertools.product(partition, [out_path], [log_path]))
 
-if(len(sys.argv)!=5 and len(sys.argv)!=6):
+if(len(sys.argv)!=4 and len(sys.argv)!=5):
 	print("Wrong arguments, please check comments in the script for usage")
 	sys.exit(1)
 
-dir_in_benign = sys.argv[1]
-dir_in_mal = sys.argv[2]
-dir_out_benign = sys.argv[3]
-dir_out_mal = sys.argv[4]
-if(len(sys.argv)==6):
-    AOSP_DIR = sys.argv[5]
+dir_in = sys.argv[1]
+dir_out = sys.argv[2]
+dir_log = sys.argv[3]
+if(len(sys.argv)==5):
+    AOSP_DIR = sys.argv[4]
 else:
     AOSP_DIR = "/data/guanxiong/android_source_7" # by default uses Android 7
 
@@ -81,5 +82,4 @@ os.environ["ANDROID_DATA"] = ANDROID_DATA
 os.environ["ANDROID_ROOT"] = ANDROID_ROOT
 os.system("mkdir -p " + ANDROID_DATA)
 os.chdir(AOSP_DIR)
-convert_files(dir_in_benign, dir_out_benign)
-convert_files(dir_in_mal, dir_out_mal)
+convert_files(dir_in, dir_out, dir_log)
