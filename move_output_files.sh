eefa38f5ba4a60f045ad63c98bd6bded05856856
ericchen321 <cgx1997@outlook.com>
2019-06-14 17:13:25 -0700

    included virus_samples and files_to_check

--- move_output_files.sh
+++ move_output_files.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# this script moves training data from bin to somewhere else
+# this script moves partitioning, training, and testing data from bin to somewhere else
 # Usage : param - folder where you want to store training data
 
+mkdir -p $1
 mv bin/benign_samples* $1
 mv bin/malware_samples* $1
+mv bin/virus_samples* $1
+mv bin/files_to_check* $1
+mv bin/results* $1
 mv bin/*.dat* $1
 mv bin/*.gle $1
-mv bin/*.txt $1
