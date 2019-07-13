31b5c6d41e772176db656b28932a3978c330dccf
ericchen321 <cgx1997@outlook.com>
2019-07-12 17:21:25 -0700

    can now take not just apk samples; allows user to specify number of samples to put in the list

--- create_file_list.py
+++ create_file_list.py
@@ -2,18 +2,23 @@
 # Usage:
 # first param: input directory
 # second param: path to output file
+# 3rd param: number of samples wanted. 0 means all
 
 import glob
 import sys
+import random
 
 in_dir = sys.argv[1]
 txt_file_path = sys.argv[2]
+num_samples = int(sys.argv[3])
 
-samples = glob.glob(in_dir + '/*.apk')
+samples = glob.glob(in_dir + '/*')
+if num_samples != 0:
+  samples = random.sample(samples, num_samples)
+
 outFile = open(txt_file_path, 'w')
-
 for line in samples:
+  line = line.replace(' ', '\ ')
   outFile.write(line)
   outFile.write('\n')
-
 outFile.close()
